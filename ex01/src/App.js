c12d46cb9e3fd8e2611a066858bf2431fd8f9d6d
Rusmira Sabic <sabic.rusmira@gmail.com>
2022-05-18 09:53:54 +0200

    Initialized state object in constructor

---
+++ ex01/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import Plant from './Plant';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Plant/>
+    </div>
+  );
+}
+
+export default App;
